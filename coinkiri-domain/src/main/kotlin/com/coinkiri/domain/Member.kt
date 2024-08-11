2812e7c1b628348929dc6d69f3e29567e0d1eb92
Yeongjae Kim <sees77770@gmail.com>
2024-08-12 00:08:23 +0900

    feat : SocialType enum class 생성 #1

--- coinkiri-domain/src/main/kotlin/com/coinkiri/domain/Member.kt
+++ coinkiri-domain/src/main/kotlin/com/coinkiri/domain/Member.kt
@@ -3,6 +3,7 @@ package com.coinkiri.domain
 data class Member(
     val id: Long,
     val socialId: String,
-    val socialType: String,
+    val socialType: SocialType,
     val nickname: String
-)
+) {
+}
---
+++ coinkiri-domain/src/main/kotlin/com/coinkiri/domain/SocialType.kt
@@ -0,0 +1,8 @@
+package com.coinkiri.domain
+
+enum class SocialType(
+    val value: String
+) {
+    KAKAO("카카오"),
+    APPLE("네이버");
+}
