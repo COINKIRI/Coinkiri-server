d1c5b775b0de5c9fee34bea48f4aea37bc1d2c2a
Yeongjae Kim <sees77770@gmail.com>
2024-08-15 10:37:49 +0900

    feat : 각 모듈 component scan config 추가

---
+++ coinkiri-infrastructure/persistence/src/main/kotlin/com/coinkiri/persistence/config/JpaConfig.kt
@@ -0,0 +1,12 @@
+package com.coinkiri.persistence.config
+
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@Configuration
+@EnableJpaAuditing
+@EntityScan(basePackages = ["com.coinkiri.persistence"])
+@EnableJpaRepositories(basePackages = ["com.coinkiri.persistence"])
+class JpaConfig
