b24d8076f4a8788a2303c706a23f0f88a2e57d36
Yeongjae Kim <sees77770@gmail.com>
2024-08-15 23:55:15 +0900

    feat : memberCreateRequest token 필드 추가

--- coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/controller/MemberController.kt
+++ coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/controller/MemberController.kt
@@ -3,22 +3,25 @@ package com.coinkiri.api.adapter.controller
 import com.coinkiri.api.adapter.request.MemberCreateRequest
 import com.coinkiri.api.adapter.response.MemberResponse
 import com.coinkiri.application.port.`in`.usecase.MemberUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Member")
 @RestController
 @RequestMapping("api/v1/members")
 class MemberController(
     private val memberUseCase: MemberUseCase
 ) {
 
+    @Operation(summary = "소셜 회원가입")
     @PostMapping("/create")
     fun create(
         @RequestBody request: MemberCreateRequest
-    ): MemberResponse {
-        val member = memberUseCase.create(request.to())
-        return MemberResponse.from(member)
+    ) {
+
     }
 }
--- coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/request/MemberCreateRequest.kt
+++ coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/request/MemberCreateRequest.kt
@@ -4,11 +4,11 @@ import com.coinkiri.domain.member.MemberCreate
 import com.coinkiri.domain.member.SocialType
 
 data class MemberCreateRequest(
-    val socialId: String,
+    val token: String,
     val socialType: String,
     val nickname: String
 ) {
-    fun to() = MemberCreate(
+    fun to(socialId: String) = MemberCreate(
         socialId = socialId,
         socialType = SocialType.valueOf(socialType),
         nickname = nickname
