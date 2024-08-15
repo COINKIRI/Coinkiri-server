abd3de15b538fb311dac56a39331fd9ab66d9952
Yeongjae Kim <sees77770@gmail.com>
2024-08-16 00:15:55 +0900

    feat : AuthUseCase port 생성

--- coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/controller/AuthController.kt
+++ coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/controller/AuthController.kt
@@ -1,8 +1,7 @@
 package com.coinkiri.api.adapter.controller
 
-import com.coinkiri.api.adapter.request.MemberCreateRequest
-import com.coinkiri.api.adapter.response.MemberResponse
-import com.coinkiri.application.port.`in`.usecase.MemberUseCase
+import com.coinkiri.api.adapter.request.SignUpRequest
+import com.coinkiri.application.port.`in`.usecase.AuthUseCase
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @Tag(name = "Member")
 @RestController
-@RequestMapping("api/v1/members")
-class MemberController(
-    private val memberUseCase: MemberUseCase
+@RequestMapping("api/v1/auth")
+class AuthController(
+    private val authUseCase: AuthUseCase
 ) {
 
     @Operation(summary = "소셜 회원가입")
-    @PostMapping("/create")
-    fun create(
-        @RequestBody request: MemberCreateRequest
+    @PostMapping("/signUp")
+    fun signUp(
+        @RequestBody request: SignUpRequest
     ) {
 
     }
--- coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/request/SignUpRequest.kt
+++ coinkiri-bootstrap/api/src/main/kotlin/com/coinkiri/api/adapter/request/SignUpRequest.kt
@@ -3,7 +3,7 @@ package com.coinkiri.api.adapter.request
 import com.coinkiri.domain.member.MemberCreate
 import com.coinkiri.domain.member.SocialType
 
-data class MemberCreateRequest(
+data class SignUpRequest(
     val token: String,
     val socialType: String,
     val nickname: String
