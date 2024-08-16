46c5f29caae59f8efebf62cf4f7adfbaee6a2918
Yeongjae Kim <sees77770@gmail.com>
2024-08-16 22:48:03 +0900

    fix : port 에 의존하도록 수정

--- coinkiri-application/src/main/kotlin/com/coinkiri/application/service/auth/KakaoAuthService.kt
+++ coinkiri-application/src/main/kotlin/com/coinkiri/application/service/auth/KakaoAuthService.kt
@@ -2,20 +2,20 @@ package com.coinkiri.application.service.auth
 
 import com.coinkiri.application.port.`in`.command.SignUpCommand
 import com.coinkiri.application.port.`in`.usecase.AuthUseCase
+import com.coinkiri.application.port.`in`.usecase.MemberUseCase
 import com.coinkiri.application.port.out.oauth2.KakaoApiCaller
-import com.coinkiri.application.service.member.MemberService
 import com.coinkiri.domain.member.Member
 import org.springframework.stereotype.Service
 
 @Service
 class KakaoAuthService(
     private val kakaoApiCaller: KakaoApiCaller,
-    private val memberService: MemberService
+    private val memberUseCase: MemberUseCase
 ) : AuthUseCase {
 
     override fun signUp(signUpCommand: SignUpCommand): Member {
         val kakaoProfile = kakaoApiCaller.getProfile(signUpCommand.token)
-        val member = memberService.create(signUpCommand.to(kakaoProfile.socialId))
+        val member = memberUseCase.create(signUpCommand.to(kakaoProfile.socialId))
         return member
     }
 }
