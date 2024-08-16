a372d395fcd790c9652ace961a23020fed98b8f1
Yeongjae Kim <sees77770@gmail.com>
2024-08-16 15:12:57 +0900

    feat : kakao getProfile 메서드 구현

--- coinkiri-application/src/main/kotlin/com/coinkiri/application/port/out/oauth2/OAuthApiCaller.kt
+++ coinkiri-application/src/main/kotlin/com/coinkiri/application/port/out/oauth2/OAuthApiCaller.kt
@@ -1,6 +1,8 @@
 package com.coinkiri.application.port.out.oauth2
 
+import com.coinkiri.application.KakaoProfileResponse
+
 interface OAuthApiCaller {
 
-    fun getProfile(accessToken: String)
+    fun getProfile(accessToken: String): KakaoProfileResponse
 }
--- coinkiri-infrastructure/oauth2/src/main/kotlin/com/coinkiri/oauth2/adapter/kakao/KakaoApiCallerAdapter.kt
+++ coinkiri-infrastructure/oauth2/src/main/kotlin/com/coinkiri/oauth2/adapter/kakao/KakaoApiCallerAdapter.kt
@@ -1,5 +1,6 @@
 package com.coinkiri.oauth2.adapter.kakao
 
+import com.coinkiri.application.KakaoProfileResponse
 import com.coinkiri.application.port.out.oauth2.OAuthApiCaller
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
@@ -9,7 +10,12 @@ class KakaoApiCallerAdapter(
     private val webClient: WebClient
 ) : OAuthApiCaller {
 
-    override fun getProfile(accessToken: String) {
-        TODO("Not yet implemented")
+    override fun getProfile(accessToken: String): KakaoProfileResponse {
+        return webClient.get()
+            .uri("https://kapi.kakao.com/v2/user/me")
+            .headers { it.setBearerAuth(accessToken) }
+            .retrieve()
+            .bodyToMono(KakaoProfileResponse::class.java)
+            .block()!!
     }
 }
