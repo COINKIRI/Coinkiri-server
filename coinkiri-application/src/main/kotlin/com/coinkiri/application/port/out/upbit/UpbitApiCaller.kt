f7cf5f5aba384ffa0e8d301ba651e76ebd9fd545
Yeongjae Kim <sees77770@gmail.com>
2024-08-28 18:43:40 +0900

    feat : 코인 상세 조회 (200일 값) 요청 구현

--- coinkiri-application/src/main/kotlin/com/coinkiri/application/port/out/upbit/UpbitApiCaller.kt
+++ coinkiri-application/src/main/kotlin/com/coinkiri/application/port/out/upbit/UpbitApiCaller.kt
@@ -5,4 +5,6 @@ import com.coinkiri.domain.coin.CoinCreate
 interface UpbitApiCaller {
 
     fun getCoinList(): List<CoinCreate>
+
+    fun getCoinDetail(market: String)
 }
--- coinkiri-infrastructure/upbit/src/main/kotlin/com/coinkiri/upbit/adapter/UpbitApiCallerAdapter.kt
+++ coinkiri-infrastructure/upbit/src/main/kotlin/com/coinkiri/upbit/adapter/UpbitApiCallerAdapter.kt
@@ -1,6 +1,7 @@
 package com.coinkiri.upbit.adapter
 
-import com.coinkiri.application.port.out.dto.CoinResponse
+import com.coinkiri.application.port.out.dto.CoinDetailDto
+import com.coinkiri.application.port.out.dto.CoinDto
 import com.coinkiri.application.port.out.upbit.UpbitApiCaller
 import com.coinkiri.common.log.Slf4JKotlinLogging.log
 import com.coinkiri.domain.coin.CoinCreate
@@ -16,20 +17,31 @@ class UpbitApiCallerAdapter(
         val coinRequestUrl = "https://api.upbit.com/v1/market/all"
 
         try {
-            val response = restTemplate.getForObject(coinRequestUrl, Array<CoinResponse>::class.java)
-                ?: throw RuntimeException("Upbit API 응답이 null입니다.")
-
-            log.info { "Response: ${response.joinToString(separator = ", ") { it.toString() }}" }
+            val response = restTemplate.getForObject(coinRequestUrl, Array<CoinDto>::class.java)
 
             val krwCoinCreateList = response.filter { it.market.startsWith("KRW-") }
                 .map { CoinCreate(it.market, it.korean_name, it.english_name) }
 
             return krwCoinCreateList
-            
+
         } catch (e: Exception) {
             log.error(e) { "Error fetching coin list" }
         }
 
         return emptyList()
+    }
+
+    override fun getCoinDetail(market: String) {
+        val coinRequestUrl = "https://api.upbit.com/v1/candles/days?market=KRW-$market&count=200"
+
+        try {
+            val response = restTemplate.getForObject(coinRequestUrl, Array<CoinDetailDto>::class.java)
+
+            log.info { response }
+
+
+        } catch (e: Exception) {
+            log.error(e) { "Error fetching coin detail" }
+        }
     }
 }
